129ed4308825418bac0e150bf48d24187782b363
opiotrowsky <piotrowsky.o@gmail.com>
2023-04-22 21:55:30 +0200

    Added tests for double flags

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch_all.hpp>
 #include "MyGrep.hpp"
-#include "main.cpp"
 
 SCENARIO("Program is looking for words with default settings.") {
     GIVEN("The word 'house'") {
@@ -20,7 +19,7 @@ SCENARIO("Program is looking for words with default settings.") {
     }
 }
 
-SCENARIO("Program is looking for words with flags.") {
+SCENARIO("Program is looking for words with single flag.") {
     GIVEN("The word 'house'") {
         std::string word = "house";
         MyGrep lookFor(word);
@@ -61,6 +60,83 @@ SCENARIO("Program is looking for words with flags.") {
             lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
 
             THEN("new threads number should be 12") {
+                REQUIRE(lookFor.getThreadsNum() == newThrdNum);
+            }
+        }
+    }
+}
+
+SCENARIO("Program is looking for word 'house' with multiple flags.") {
+    GIVEN("The word 'house'") {
+        std::string word = "house";
+        MyGrep lookFor(word);
+        std::string newDir;
+        std::string newLogFile;
+        std::string newResFile;
+        int newThrdNum;
+
+        WHEN("'-d' and '-l' flags are set") {
+            newDir = "c/users/download";
+            newLogFile = "newLog.log";
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("searched directory and log file name should be changed") {
+                REQUIRE(lookFor.getSearchedDir() == newDir);
+                REQUIRE(lookFor.getLogFileName() == newLogFile);
+            }
+        }
+
+        WHEN("'-d' and '-r' flags are set") {
+            newDir = "c/users/download";
+            newResFile = "newRes.txt";
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("searched directory and result file name should be changed") {
+                REQUIRE(lookFor.getSearchedDir() == newDir);
+                REQUIRE(lookFor.getResFileName() == newResFile);
+            }
+        }
+
+        WHEN("'-d' and '-t' flags are set") {
+            newDir = "c/users/download";
+            newThrdNum = 12;
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("searched directory and thread number should be changed") {
+                REQUIRE(lookFor.getSearchedDir() == newDir);
+                REQUIRE(lookFor.getThreadsNum() == newThrdNum);
+            }
+        }
+
+        WHEN("'-l' and '-r' flags are set") {
+            newLogFile = "newLog.log";
+            newResFile = "newRes.txt";
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("log file and result file names should be changed") {
+                REQUIRE(lookFor.getLogFileName() == newLogFile);
+                REQUIRE(lookFor.getResFileName() == newResFile);
+            }
+        }
+
+        WHEN("'-l' and '-t' flags are set") {
+            newLogFile = "newLog.log";
+            newThrdNum = 12;
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("log file name and thread number should be changed") {
+                REQUIRE(lookFor.getLogFileName() == newLogFile);
+                REQUIRE(lookFor.getThreadsNum() == newThrdNum);
+            }
+        }
+
+        WHEN("'-r' and '-t' flags are set") {
+            newResFile = "newRes.txt";
+            newThrdNum = 12;
+            lookFor.setFlags(newDir, newLogFile, newResFile, newThrdNum);
+
+            THEN("res file name and thread number should be changed") {
+                REQUIRE(lookFor.getResFileName() == newResFile);
                 REQUIRE(lookFor.getThreadsNum() == newThrdNum);
             }
         }
